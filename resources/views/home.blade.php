fba470753be3e43d74bd617404b7a2d9e1c07150
Laure <dominguezlaure@gmail.com>
2023-03-03 01:39:00 +0100

    improved home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,141 +4,146 @@
     </x-slot>
 
     <x-slot name="header">
-        <h1>{{ __('Les Pâtes du chat') }}</h1>
+        <h1 class="titre">{{ __('Les Pâtes du chat ') }}</h1>
     </x-slot>
 
     <x-slot name="main">
-        <section class="home">
-            <figure>
-                <img src="/images/logo.png" alt="logo" class="logo">
-                <figcaption>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, nemo ullam! Accusamus necessitatibus enim odit laudantium esse iste fugit, nemo vel accusantium quod numquam voluptates voluptate voluptas, doloribus, excepturi reprehenderit.
-                </figcaption>
-            </figure>
-        </section>
-            
-        <section>
-            <h2>News</h2>
+        <div class="container">
 
-            <div id="album-rotator" class="album-rotator">
-                <div id="album-rotator-holder" class="album-rotator-holder">
-                    <a target="_top" class="album-item" href="">
-                        <img src="/images/1677165410.png" alt="">
-                        <p class="album-details">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
-                        </p>
-                    </a>
-                    <a target="_top" class="album-item" href="">
-                        <img src="https://picsum.photos/300/200" alt="">
-                        <p class="album-details">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
-                        </p>
-                    </a>
-                    <a target="_top" class="album-item" href="">
-                        <img src="https://picsum.photos/300/200" alt="">
-                        <p class="album-details">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
-                        </p>
-                    </a>
-                    <a target="_top" class="album-item" href="">
-                        <img src="https://picsum.photos/300/200" alt="">
-                        <p class="album-details">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
-                        </p>
-                    </a>
-                    <a class="album-item" href="">
-                        <img src="https://picsum.photos/300/200" alt="">
-                        <p class="album-details">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
-                        </p>
-                    </a>
-                </div>
-            </div>
-        </section>
-
-        <section>
-            <h2>Shop</h2>
-            <div id="album-rotator" class="album-rotator">
-                <div id="album-rotator-holder" class="album-rotator-holder">
-                    <a target="_top" class="album-item" href="">
-                        <img src="/images/1677165410.png" alt="">
-                        <p class="album-details">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
-                        </p>
-                    </a>
-                    <a target="_top" class="album-item" href="">
-                        <img src="https://picsum.photos/300/200" alt="">
-                        <p class="album-details">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
-                        </p>
-                    </a>
-                    <a target="_top" class="album-item" href="">
-                        <img src="https://picsum.photos/300/200" alt="">
-                        <p class="album-details">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
-                        </p>
-                    </a>
-                    <a target="_top" class="album-item" href="">
-                        <img src="https://picsum.photos/300/200" alt="">
-                        <p class="album-details">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
-                        </p>
-                    </a>
-                    <a class="album-item" href="">
-                        <img src="https://picsum.photos/300/200" alt="">
-                        <p class="album-details">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
-                        </p>
-                    </a>
-                </div>
-            </div>
-        </section>
-
-        <section>
-            <h2>Recettes</h2>
-            <div id="album-rotator" class="album-rotator">
-                <div id="album-rotator-holder" class="album-rotator-holder">
-                    <a target="_top" class="album-item" href="">
-                        <img src="/images/1677165410.png" alt="">
-                        <p class="album-details">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
-                        </p>
-                    </a>
-                    <a target="_top" class="album-item" href="">
-                        <img src="https://picsum.photos/300/200" alt="">
-                        <p class="album-details">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
-                        </p>
-                    </a>
-                    <a target="_top" class="album-item" href="">
-                        <img src="https://picsum.photos/300/200" alt="">
-                        <p class="album-details">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
-                        </p>
-                    </a>
-                    <a target="_top" class="album-item" href="">
-                        <img src="https://picsum.photos/300/200" alt="">
-                        <p class="album-details">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
-                        </p>
-                    </a>
-                    <a class="album-item" href="">
-                        <img src="https://picsum.photos/300/200" alt="">
-                        <p class="album-details">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
-                        </p>
-                    </a>
-                </div>
-            </div>
-        </section>
-
-        <section>
-            <h2>contact</h2>
-            <div>contact + map</div>
-            <table>
+            <section class="home">
+                <figure>
+                    <img src="/images/pasta-flour_I4MIIMHZ5M.jpg">
+                    <figcaption>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, nemo ullam! Accusamus necessitatibus enim odit laudantium esse iste fugit, nemo vel accusantium quod numquam voluptates voluptate voluptas, doloribus, excepturi reprehenderit. <br>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore a molestiae tenetur, accusantium similique corrupti ratione fugit voluptatibus dolores expedita odio quas magnam rerum dignissimos harum voluptatem facilis voluptatum quos! <br>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. A dolorum ratione consequuntur blanditiis explicabo natus omnis, id, incidunt quae laborum magni ipsam, exercitationem quidem sequi obcaecati minima ex hic illum!
+                    </figcaption>
+                </figure>
+            </section>
                 
-            </table>
-        </section>
+            <section>
+                <h2>News</h2>
+
+                <div id="album-rotator" class="album-rotator">
+                    <div id="album-rotator-holder" class="album-rotator-holder">
+                        <a target="_top" class="album-item" href="">
+                            <img src="/images/1677165410.png" alt="">
+                            <p class="album-details">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
+                            </p>
+                        </a>
+                        <a target="_top" class="album-item" href="">
+                            <img src="https://picsum.photos/300/200" alt="">
+                            <p class="album-details">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
+                            </p>
+                        </a>
+                        <a target="_top" class="album-item" href="">
+                            <img src="https://picsum.photos/300/200" alt="">
+                            <p class="album-details">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
+                            </p>
+                        </a>
+                        <a target="_top" class="album-item" href="">
+                            <img src="https://picsum.photos/300/200" alt="">
+                            <p class="album-details">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
+                            </p>
+                        </a>
+                        <a class="album-item" href="">
+                            <img src="https://picsum.photos/300/200" alt="">
+                            <p class="album-details">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
+                            </p>
+                        </a>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <h2>Shop</h2>
+                <div id="album-rotator" class="album-rotator">
+                    <div id="album-rotator-holder" class="album-rotator-holder">
+                        <a target="_top" class="album-item" href="">
+                            <img src="/images/1677165410.png" alt="">
+                            <p class="album-details">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
+                            </p>
+                        </a>
+                        <a target="_top" class="album-item" href="">
+                            <img src="https://picsum.photos/300/200" alt="">
+                            <p class="album-details">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
+                            </p>
+                        </a>
+                        <a target="_top" class="album-item" href="">
+                            <img src="https://picsum.photos/300/200" alt="">
+                            <p class="album-details">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
+                            </p>
+                        </a>
+                        <a target="_top" class="album-item" href="">
+                            <img src="https://picsum.photos/300/200" alt="">
+                            <p class="album-details">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
+                            </p>
+                        </a>
+                        <a class="album-item" href="">
+                            <img src="https://picsum.photos/300/200" alt="">
+                            <p class="album-details">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
+                            </p>
+                        </a>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <h2>Recettes</h2>
+                <div id="album-rotator" class="album-rotator">
+                    <div id="album-rotator-holder" class="album-rotator-holder">
+                        <a target="_top" class="album-item" href="">
+                            <img src="/images/1677165410.png" alt="">
+                            <p class="album-details">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
+                            </p>
+                        </a>
+                        <a target="_top" class="album-item" href="">
+                            <img src="https://picsum.photos/300/200" alt="">
+                            <p class="album-details">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
+                            </p>
+                        </a>
+                        <a target="_top" class="album-item" href="">
+                            <img src="https://picsum.photos/300/200" alt="">
+                            <p class="album-details">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
+                            </p>
+                        </a>
+                        <a target="_top" class="album-item" href="">
+                            <img src="https://picsum.photos/300/200" alt="">
+                            <p class="album-details">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
+                            </p>
+                        </a>
+                        <a class="album-item" href="">
+                            <img src="https://picsum.photos/300/200" alt="">
+                            <p class="album-details">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti necessitatibus ipsum dolores sint, eveniet corrupti et. Aut eligendi expedita quae exercitationem nemo dignissimos vero aperiam quasi? Suscipit commodi enim deserunt!
+                            </p>
+                        </a>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <h2>contact</h2>
+                <div>contact + map</div>
+                <table>
+                    
+                </table>
+            </section>
+        </div>
     </x-slot>
 
 </x-app-layout>
