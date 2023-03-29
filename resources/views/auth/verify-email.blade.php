7f133414a2c6159a06aba365c948c49934c863b0
Laure <dominguezlaure@gmail.com>
2023-03-29 09:41:16 +0200

    modifyed login template

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,11 +1,11 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+        {{ __('Merci pour votre inscription ! Avant de commencer, pouvez-vous valider votre addresse email en cliquant sur le lien qui vous a été envoyé par mail ? Si vous n\'avez rien reçu, nous pouvons vous envoyer un nouveau lien.') }}
     </div>
 
     @if (session('status') == 'verification-link-sent')
         <div class="mb-4 font-medium text-sm text-green-600">
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+            {{ __('Un nouveau lien de validation a été envoyé à l\'adresse que vous avez utilisé pour la création de votre compte.') }}
         </div>
     @endif
 
@@ -15,7 +15,7 @@
 
             <div>
                 <x-primary-button>
-                    {{ __('Resend Verification Email') }}
+                    {{ __('Envoyer un nouveau lien') }}
                 </x-primary-button>
             </div>
         </form>
@@ -24,7 +24,7 @@
             @csrf
 
             <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                {{ __('Log Out') }}
+                {{ __('Déconnexion') }}
             </button>
         </form>
     </div>
